54dcf713091ef668998ebed70ad36f24973644df
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 17:24:27 +0100

    Add dummy mastercard_payment_gateway domain service

--- payments/lib/payments.rb
+++ payments/lib/payments.rb
@@ -7,3 +7,4 @@ require_dependency 'payments/domain_events/credit_card_authorization_succeeded.r
 require_dependency 'payments/domain_events/credit_card_authorization_failed.rb'
 
 require_dependency 'payments/domain_services/visa_payment_gateway.rb'
+require_dependency 'payments/domain_services/mastercard_payment_gateway.rb'
---
+++ payments/lib/payments/domain_services/mastercard_payment_gateway.rb
@@ -0,0 +1,40 @@
+module Payments
+  class MastercardPaymentGateway
+    class Transaction < ActiveRecord::Base
+      self.table_name = 'mastercard_payment_gateway_transactions'
+    end
+    private_constant :Transaction
+
+    AuthorizationFailed = Class.new(StandardError)
+    CaptureFailed       = Class.new(StandardError)
+    VoidFailed          = Class.new(StandardError)
+
+    def authorize(credit_card_token, amount, currency)
+      transaction_identifier = SecureRandom.hex(25)
+      Transaction.create!(
+        identifier:        transaction_identifier,
+        credit_card_token: credit_card_token,
+        amount:            amount,
+        currency:          currency,
+        state:             'authorized'
+      )
+      transaction_identifier
+    end
+
+    def capture(transaction_identifier)
+      transaction = Transaction.find_by(identifier: transaction_identifier, state: 'authorized')
+      raise CaptureFailed unless transaction
+      transaction.state = 'captured'
+      transaction.save!
+      true
+    end
+
+    def void(transaction_identifier)
+      transaction = Transaction.find_by(identifier: transaction_identifier, state: 'authorized')
+      raise VoidFailed unless transaction
+      transaction.state = 'voided'
+      transaction.save!
+      true
+    end
+  end
+end
