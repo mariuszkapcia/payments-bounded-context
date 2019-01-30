932fc785178ab5e4d2975a860fe5502d0da8c7cf
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 17:23:32 +0100

    Add dummy visa_payment_gateway domain service

--- payments/lib/payments.rb
+++ payments/lib/payments.rb
@@ -5,3 +5,5 @@ require_dependency 'payments/commands/authorize_credit_card.rb'
 
 require_dependency 'payments/domain_events/credit_card_authorization_succeeded.rb'
 require_dependency 'payments/domain_events/credit_card_authorization_failed.rb'
+
+require_dependency 'payments/domain_services/visa_payment_gateway.rb'
