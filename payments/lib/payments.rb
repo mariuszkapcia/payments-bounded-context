52839fba914adcb71627815ec01be2f2c5d31457
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 19:53:42 +0100

    Add capture_succeeded and capture_failed domain events

--- payments/lib/payments.rb
+++ payments/lib/payments.rb
@@ -10,6 +10,8 @@ require_dependency 'payments/commands/capture_authorization.rb'
 
 require_dependency 'payments/domain_events/credit_card_authorization_succeeded.rb'
 require_dependency 'payments/domain_events/credit_card_authorization_failed.rb'
+require_dependency 'payments/domain_events/capture_succeeded.rb'
+require_dependency 'payments/domain_events/capture_failed.rb'
 
 require_dependency 'payments/domain_services/visa_payment_gateway.rb'
 require_dependency 'payments/domain_services/mastercard_payment_gateway.rb'
