e92115338f06444468de77f0044d9c7899975aac
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 21:08:53 +0100

    Add basic validations to order aggregate

--- orders/lib/orders/aggregates/order.rb
+++ orders/lib/orders/aggregates/order.rb
@@ -2,12 +2,16 @@ module Orders
   class Order
     include AggregateRoot
 
+    NotAllowed = Class.new(StandardError)
+
     def initailize(uuid)
       @state  = :draft
       @uuid   = uuid
     end
 
     def submit(order_number)
+      raise NotAllowed unless draft?
+
       apply(Orders::OrderSubmitted.strict(data: {
         order_uuid:   @uuid,
         order_number: order_number
@@ -15,12 +19,27 @@ module Orders
     end
 
     def ship
+      raise NotAllowed unless submitted?
+      raise NotAllowed if shipped?
+
       apply(Orders::OrderShipped.strict(data: {
         order_uuid: @uuid
       }))
     end
 
     private
+
+    def draft?
+      @state == :draft
+    end
+
+    def submitted?
+      @state == :submitted
+    end
+
+    def shipped?
+      @state == :shipped
+    end
 
     def apply_order_submitted(event)
       @state = :submitted
