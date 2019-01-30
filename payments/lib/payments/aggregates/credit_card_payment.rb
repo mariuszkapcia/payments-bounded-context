e7cef0eab9e790bf65eca8a0006df2a49e55b4ef
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 17:50:29 +0100

    Add validation to authorize on credit_card_payment aggregate

--- payments/lib/payments/aggregates/credit_card_payment.rb
+++ payments/lib/payments/aggregates/credit_card_payment.rb
@@ -2,12 +2,17 @@ module Payments
   class CreditCardPayment
     include AggregateRoot
 
+    InvalidOperation = Class.new(StandardError)
+
     def initialize(transaction_identifier, payment_gateway:)
       @transaction_identifier = transaction_identifier
       @payment_gateway        = payment_gateway
+      @state                  = :unauthorized
     end
 
     def authorize(credit_card_token, amount, currency, order_number)
+      raise InvalidOperation if authorized?
+
       transaction_identifier = @payment_gateway.authorize(credit_card_token, amount, currency)
 
       # NOTE: Should I include amount and currency here?
@@ -25,8 +30,13 @@ module Payments
 
     private
 
+    def authorized?
+      @state == :authorized
+    end
+
     def apply_credit_card_authorization_succeeded(event)
-      @state = :authorized
+      @transaction_identifier = event.data[:transaction_identifier]
+      @state                  = :authorized
     end
   end
 end
