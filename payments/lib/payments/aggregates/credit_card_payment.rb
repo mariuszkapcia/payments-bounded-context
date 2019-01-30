07b0a0b59fe6bef9cb80bbb87acf6b5ddaf8c134
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 18:22:41 +0100

    Add payment_gateway_transaction_identifier to authorize method

--- payments/lib/payments/aggregates/credit_card_payment.rb
+++ payments/lib/payments/aggregates/credit_card_payment.rb
@@ -5,22 +5,24 @@ module Payments
     InvalidOperation = Class.new(StandardError)
 
     def initialize(transaction_identifier, payment_gateway:)
-      @transaction_identifier = transaction_identifier
-      @payment_gateway        = payment_gateway
-      @state                  = :unauthorized
+      @transaction_identifier                 = transaction_identifier
+      @payment_gateway_transaction_identifier = nil
+      @payment_gateway                        = payment_gateway
+      @state                                  = :unauthorized
     end
 
     def authorize(credit_card_token, amount, currency, order_number)
       raise InvalidOperation if authorized?
 
-      transaction_identifier = @payment_gateway.authorize(credit_card_token, amount, currency)
+      payment_gateway_transaction_identifier = @payment_gateway.authorize(credit_card_token, amount, currency)
 
       # NOTE: Should I include amount and currency here?
       apply(Payments::CreditCardAuthorizationSucceeded.strict(data: {
-        transaction_identifier: transaction_identifier,
-        order_number:           order_number,
-        amount:                 amount,
-        currency:               currency
+        transaction_identifier:                 @transaction_identifier,
+        payment_gateway_transaction_identifier: payment_gateway_transaction_identifier,
+        order_number:                           order_number,
+        amount:                                 amount,
+        currency:                               currency
       }))
     rescue Payments::AuthorizationFailed
       apply(Payments::CreditCardAuthorizationFailed.strict(data: {
@@ -35,8 +37,9 @@ module Payments
     end
 
     def apply_credit_card_authorization_succeeded(event)
-      @transaction_identifier = event.data[:transaction_identifier]
-      @state                  = :authorized
+      @transaction_identifier                 = event.data[:transaction_identifier]
+      @payment_gateway_transaction_identifier = event.data[:payment_gateway_transaction_identifier]
+      @state                                  = :authorized
     end
   end
 end
