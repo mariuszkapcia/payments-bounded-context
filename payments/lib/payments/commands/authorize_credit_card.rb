706370c68ec87032733f3ca89e299d4f2a32ed62
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 17:15:12 +0100

    Add currency to authorize_credit_card command

--- payments/lib/payments/commands/authorize_credit_card.rb
+++ payments/lib/payments/commands/authorize_credit_card.rb
@@ -4,11 +4,13 @@ module Payments
 
     attr_accessor :credit_card_token
     attr_accessor :amount
+    attr_accessor :currency
     attr_accessor :order_number
 
-    def initialize(credit_card_token:, amount:, order_number:)
+    def initialize(credit_card_token:, amount:, currency:, order_number:)
       @credit_card_token = credit_card_token
       @amount            = amount
+      @currency          = currency
       @order_number      = order_number
     end
   end
