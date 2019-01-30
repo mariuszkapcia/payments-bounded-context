0e9ef9de7c91616def297bea4eae5fe17cb87719
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 20:59:15 +0100

    Add submit_order command

--- orders/lib/orders.rb
+++ orders/lib/orders.rb
@@ -3,5 +3,7 @@ end
 
 require_dependency 'orders/aggregates/order.rb'
 
+require_dependency 'orders/commands/submit_order.rb'
+
 require_dependency 'orders/domain_events/order_submitted.rb'
 require_dependency 'orders/domain_events/order_shipped.rb'
