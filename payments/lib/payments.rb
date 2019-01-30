7c267f04bef85d53fe627fe56ce6dedc3c302c50
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 16:59:12 +0100

    Add authorize_credit_card command

--- payments/lib/payments.rb
+++ payments/lib/payments.rb
@@ -1,5 +1,7 @@
 module Payments
 end
 
+require_dependency 'payments/commands/authorize_credit_card.rb'
+
 require_dependency 'payments/domain_events/credit_card_authorization_succeeded.rb'
 require_dependency 'payments/domain_events/credit_card_authorization_failed.rb'
---
+++ payments/lib/payments/commands/authorize_credit_card.rb
@@ -0,0 +1,13 @@
+module Payments
+  class AuthorizeCreditCard
+    attr_accessor :credit_card_token
+    attr_accessor :amount
+    attr_accessor :order_number
+
+    def initialize(credit_card_token:, amount:, order_number:)
+      @credit_card_token = credit_card_token
+      @amount            = amount
+      @order_number      = order_number
+    end
+  end
+end
