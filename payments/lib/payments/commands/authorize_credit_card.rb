d44c1c7610838b32c3d8bab8d290165015194bdc
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-30 18:18:52 +0100

    Add transaction_identifier to authorize_credit_card command

--- payments/lib/payments/commands/authorize_credit_card.rb
+++ payments/lib/payments/commands/authorize_credit_card.rb
@@ -2,16 +2,18 @@ module Payments
   class AuthorizeCreditCard
     include Command
 
+    attr_accessor :transaction_identifier
     attr_accessor :credit_card_token
     attr_accessor :amount
     attr_accessor :currency
     attr_accessor :order_number
 
-    def initialize(credit_card_token:, amount:, currency:, order_number:)
-      @credit_card_token = credit_card_token
-      @amount            = amount
-      @currency          = currency
-      @order_number      = order_number
+    def initialize(transaction_identifier:, credit_card_token:, amount:, currency:, order_number:)
+      @transaction_identifier = transaction_identifier
+      @credit_card_token      = credit_card_token
+      @amount                 = amount
+      @currency               = currency
+      @order_number           = order_number
     end
   end
 end
