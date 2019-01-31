0653ecc8e7b8fccac9dcd22ff71eea296f38f5b1
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-01-31 18:09:36 +0100

    Add command_bus config

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -9,4 +9,14 @@ Rails.configuration.to_prepare do
   AggregateRoot.configure do |config|
     config.default_event_store = event_store
   end
+
+  # Payments commands.
+  command_bus.register(Payments::AuthorizeCreditCard, Payments::OnAuthorizeCreditCard.new(event_store))
+  command_bus.register(Payments::CaptureAuthorization, Payments::OnCaptureAuthorization.new(event_store))
+  command_bus.register(Payments::VoidAuthorization, Payments::OnVoidAuthorization.new(event_store))
+
+  # Orders commands.
+  command_bus.register(Orders::SubmitOrder, Orders::OnSubmitOrder.new(event_store))
+  command_bus.register(Orders::ShipOrder, Orders::OnShipOrder.new(event_store))
+  command_bus.register(Orders::CancelOrder, Orders::OnCancelOrder.new(event_store))
 end
