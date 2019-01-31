87644c91efc2cfbcb4a34e8bc49cdad1773476d0
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-02-01 00:15:32 +0100

    Fix on_capture_authorization command handler

--- payments/spec/command_handlers/on_capture_authorization_spec.rb
+++ payments/spec/command_handlers/on_capture_authorization_spec.rb
@@ -3,7 +3,7 @@ require_dependency 'payments'
 module Payments
   RSpec.describe 'OnCaptureAuthorization command handler' do
     specify 'capture authorization' do
-      command_bus = command_bus_factory
+      command_bus = command_bus_factory(payment_gateway_list: FakePaymentGatewayList.new)
       command_bus.call(AuthorizeCreditCard.new(
         transaction_identifier: transaction_identifier,
         credit_card_token:      credit_card_token,
@@ -19,6 +19,29 @@ module Payments
     end
 
     private
+
+    class FakePaymentGateway
+      def authorize(credit_card_token, amount, currency)
+        'payment_gateway_transaction_identifier'
+      end
+
+      def capture(transaction_identifier)
+      end
+
+      def identifier
+        'fake'
+      end
+    end
+
+    class FakePaymentGatewayList
+      def fetch_primary
+        FakePaymentGateway.new
+      end
+
+      def find(identifier)
+        FakePaymentGateway.new
+      end
+    end
 
     def capture_succeeded
       an_event(Payments::CaptureSucceeded).with_data(capture_succeeded_data).strict
@@ -61,10 +84,16 @@ module Payments
       end
     end
 
-    def command_bus_factory
+    def command_bus_factory(payment_gateway_list:)
       Arkency::CommandBus.new.tap do |bus|
-        bus.register(AuthorizeCreditCard, OnAuthorizeCreditCard.new(event_store))
-        bus.register(CaptureAuthorization, OnCaptureAuthorization.new(event_store))
+        bus.register(AuthorizeCreditCard, OnAuthorizeCreditCard.new(
+          event_store,
+          payment_gateway_list: payment_gateway_list
+        ))
+        bus.register(CaptureAuthorization, OnCaptureAuthorization.new(
+          event_store,
+          payment_gateway_list: payment_gateway_list
+        ))
       end
     end
   end
